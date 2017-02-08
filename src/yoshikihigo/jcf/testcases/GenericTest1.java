545cc61113162029aa80824fdd389746bbf82d63
higo <higo@192.168.1.5>
2017-02-09 00:28:53 +0900

    test cases were added.

---
+++ src/yoshikihigo/jcf/testcases/GenericTest1.java
@@ -0,0 +1,15 @@
+package yoshikihigo.jcf.testcases;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class GenericTest1 {
+
+	void method1() {
+		new HashSet<Set<Set<String>>>();
+		//new HashMap<String, Map<String, List<String>>>();
+	}
+}
---
+++ src/yoshikihigo/jcf/testcases/OverrideTest3.java
@@ -0,0 +1,8 @@
+package yoshikihigo.jcf.testcases;
+
+public class OverrideTest3 {
+
+	@Override
+	protected void finalize() throws Throwable {
+	}
+}
