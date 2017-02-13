225f143e0cc9bfc3b2bebf146fd18f8c944f23db
yoshi <yoshi@192.168.146.113>
2017-02-13 10:47:25 +0900

    add a test case

---
+++ src/yoshikihigo/jcf/testcases/InnerClassTest1.java
@@ -0,0 +1,20 @@
+package yoshikihigo.jcf.testcases;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class InnerClassTest1 extends JButton {
+
+	void doSomething() {
+		this.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				final int index = "ABCDE".substring(3).length();
+			}
+
+		});
+	}
+}
