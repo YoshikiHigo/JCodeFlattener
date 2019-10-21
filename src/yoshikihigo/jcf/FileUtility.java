f97157b176356045747002b220fc21df4c9fdc89
Yoshiki Higo <yoshiki.higo@gmail.com>
2019-10-22 00:24:45 +0900

    remove Nonnull

--- src/yoshikihigo/jcf/FileUtility.java
+++ src/yoshikihigo/jcf/FileUtility.java
@@ -9,74 +9,74 @@ import java.io.OutputStream;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
-import com.sun.istack.internal.NotNull;
-
 public class FileUtility {
 
-	public static String readFile(final File file, final String encoding) {
+  public static String readFile(final File file, final String encoding) {
 
-		final StringBuilder text = new StringBuilder();
+    final StringBuilder text = new StringBuilder();
 
-		try (final InputStreamReader reader = new InputStreamReader(new FileInputStream(file),
-				null != encoding ? encoding : "JISAutoDetect")) {
+    try (final InputStreamReader reader = new InputStreamReader(new FileInputStream(file),
+        null != encoding ? encoding : "JISAutoDetect")) {
 
-			while (reader.ready()) {
-				final int c = reader.read();
-				text.append((char) c);
-			}
+      while (reader.ready()) {
+        final int c = reader.read();
+        text.append((char) c);
+      }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
-		return text.toString();
-	}
+    return text.toString();
+  }
 
-	public static void writeFile(final String text, final String path) {
+  public static void writeFile(final String text, final String path) {
 
-		final File file = new File(path);
-		if ((null != file.getParentFile()) && !file.getParentFile().exists()) {
-			final boolean made = file.getParentFile().mkdirs();
-			assert made : "illegal state.";
-		}
+    final File file = new File(path);
+    if ((null != file.getParentFile()) && !file.getParentFile()
+        .exists()) {
+      final boolean made = file.getParentFile()
+          .mkdirs();
+      assert made : "illegal state.";
+    }
 
-		try (final OutputStream out = new FileOutputStream(path)) {
+    try (final OutputStream out = new FileOutputStream(path)) {
 
-			for (int i = 0; i < text.length(); i++) {
-				out.write(text.charAt(i));
-			}
-			out.flush();
+      for (int i = 0; i < text.length(); i++) {
+        out.write(text.charAt(i));
+      }
+      out.flush();
 
-		} catch (final IOException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
-	}
+    } catch (final IOException e) {
+      e.printStackTrace();
+      System.exit(0);
+    }
+  }
 
-	@NotNull
-	public static SortedSet<File> getFiles(final File file) {
+  public static SortedSet<File> getFiles(final File file) {
 
-		final SortedSet<File> files = new TreeSet<>();
+    final SortedSet<File> files = new TreeSet<>();
 
-		if (file.isDirectory()) {
-			final File[] children = file.listFiles();
-			if (null != children) {
-				for (final File child : children) {
-					files.addAll(getFiles(child));
-				}
-			}
-		}
+    if (file.isDirectory()) {
+      final File[] children = file.listFiles();
+      if (null != children) {
+        for (final File child : children) {
+          files.addAll(getFiles(child));
+        }
+      }
+    }
 
-		else if (file.isFile()) {
-			if (file.getName().endsWith(".java")) {
-				files.add(file);
-			}
-		}
+    else if (file.isFile()) {
+      if (file.getName()
+          .endsWith(".java")) {
+        files.add(file);
+      }
+    }
 
-		else {
-			System.err.println("\"" + file.getAbsolutePath() + "\" is not a vaild file!");
-		}
+    else {
+      System.err.println("\"" + file.getAbsolutePath() + "\" is not a vaild file!");
+    }
 
-		return files;
-	}
+    return files;
+  }
 }
